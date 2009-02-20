e0c597ee893580ca841699fd10ecd05621633413
Francisco Tufro <contacto@franciscotufro.com.ar>
2009-02-20 03:25:26 +0000

    Freezed rails with version 2.2.2

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,16 +10,13 @@ set :deploy_to, "/var/www/rails/#{application}"
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "dev.agilar.org"
-role :web, "dev.agilar.org"
-role :db,  "dev.agilar.org", :primary => true
+server "dev.agilar.org", :app, :web, :db, :primary => true
 
 namespace :deploy do
    desc "Restarting mod_rails with restart.txt"
    task :restart, :roles => :app, :except => { :no_release => true } do
      run "touch #{current_path}/tmp/restart.txt"
    end
-  endprimary => true
 
   desc "Change owner group for created directories"
   task :update_owner_group do
