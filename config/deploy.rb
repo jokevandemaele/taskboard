592fe542ab57f989fab63f5ffa1264d71a14bc38
Francisco Tufro <contacto@franciscotufro.com.ar>
2009-02-20 03:03:20 +0000

    Capistrano deploy.rb modifications

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,3 +13,18 @@ set :deploy_to, "/var/www/rails/#{application}"
 role :app, "dev.agilar.org"
 role :web, "dev.agilar.org"
 role :db,  "dev.agilar.org", :primary => true
+
+namespace :deploy do
+   desc "Restarting mod_rails with restart.txt"
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "touch #{current_path}/tmp/restart.txt"
+   end
+  endprimary => true
+
+  desc "Change owner group for created directories"
+  task :update_owner_group do
+    sudo "chown -R apache: #{deploy_to}"
+  end
+
+  after "deploy:setup", "deploy:update_owner_group"
+end
