b2e76f981b71ef6a06b50592b3262e8407a7f8c2
marquete <marquete@gmail.com>
2009-06-08 12:04:51 +0000

    * Member color picker.

---
+++ public/javascripts/color_picker.js
@@ -0,0 +1,57 @@
+function toggle_color_picker() {
+  $('admin-div-color-picker').style.display = ($('admin-div-color-picker').style.display == 'none') ? '' : 'none';
+}
+
+function dec2hex(dec) {
+  var hexits = '0123456789abcdef';
+  var hex = '';
+  if(dec == 0)
+    return '00';
+  while(dec > 0) {
+    hex = hexits.charAt(dec % 16) + hex;
+    dec = Math.floor(dec / 16);
+  }
+  if(hex.length % 2 != 0)
+    hex = '0' + hex;
+  return hex;
+}
+
+function rgb2hex(rgb) {
+  var colors = rgb.substring(4, rgb.length - 1).split(', ');
+  return '' + dec2hex(colors[0]) + dec2hex(colors[1]) + dec2hex(colors[2]);
+}
+
+function set_member_color(html_object) {
+  var color = html_object.style.backgroundColor;
+  if(color.charAt(0) == '#')
+    color = color.substr(1);
+  else
+    color = rgb2hex(color);
+  $('member_color').style.backgroundColor = $('member_color').style.color = '#' + color;
+  $('member_color').value = color;
+}
+
+function generate_color_table() {
+  var content = '<table onclick="toggle_color_picker();"><tbody>';
+  var color;
+  var i, j, k, colors_index = 0;
+  var colors = new Array('ff0000', 'd70000', 'af0000', '870000', '5f0000', '370000',
+                          'ffff00', 'd7d700', 'afaf00', '878700', '5f5f00', '373700',
+                          '00ff00', '00d700', '00af00', '008700', '005f00', '003700',
+                          '00ffff', '00d7d7', '00afaf', '008787', '005f5f', '003737',
+                          '0000ff', '0000d7', '0000af', '000087', '00005f', '000037',
+                          'ff00ff', 'd700d7', 'af00af', '870087', '5f005f', '370037'
+                          );
+
+  colors_index = 0;
+  for(i = 0; i < 6; i++) {
+    content += '<tr>';
+    for(j = 0; j < 6; j++) {
+          content += '<td onmouseover="set_member_color(this);" style="background: #' + colors[colors_index++] + '">&nbsp;</td>';
+    }
+    content += '</tr>';
+  }
+  content += '</tbody></table>';
+  return content;
+}
+
