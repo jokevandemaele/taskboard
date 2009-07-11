fee8df8f1f2f2916d2a9d910e87cef7c0408673b
Francisco Tufro <contacto@franciscotufro.com.ar>
2009-07-11 04:16:04 +0000

    * Some minor visual changes.

--- app/helpers/teams_helper.rb
+++ app/helpers/teams_helper.rb
@@ -1,2 +1,10 @@
 module TeamsHelper
+  def options_for_project_select(projects)
+    output = ''
+    projects.each do |project|
+      output += "<option value=\"#{project.id}\">#{project.name} (#{project.organization.name})</option>"
+    end
+    return output
+  end
+
 end
