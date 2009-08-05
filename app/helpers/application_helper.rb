e05b37e5b0f19bb8adf4b32f42fc5e1321428776
Francisco Tufro <contacto@franciscotufro.com.ar>
2009-08-05 17:25:34 +0000

    * Add white bkg in forms & text must be selected  * Form refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,6 +55,21 @@ module ApplicationHelper
     <div class=\"admin-div-bottom\" style=\"width: #{size}px\"><span class=\"admin-div-bottom-left\"></span><span class=\"admin-div-bottom-middle\" style=\"width: #{size-30}px\"></span><span class=\"admin-div-bottom-right\"></span></div>"
   end
   
+  def admin_form_name(element,edit, f)
+    element_class = element.class.to_s
+    name = (edit || defined? no_refresh) ? element.name : "Click To Edit #{element_class} Name"
+		f.text_field :name, 
+			:value => name, 
+			:class => "form-name name-field", 
+			:onClick => "if(this.value == 'Click To Edit #{element_class} Name' ) this.clear();" 
+  end
+  def admin_form_buttons(element)
+    element_class = element.class.to_s.downcase
+    "<div style='float: right; position:relative; top: 20px;'>
+		#{image_submit_tag 'admin/admin-form-submit.png', :alt => 'Submit', :style => "width: 50px;"}
+		#{image_tag 'admin/admin-form-cancel.png', :alt => 'Cancel', :style => "width: 45px;", :class => 'admin-form-cancel', :onClick => 'cancelForm(\'form-add-'+ element_class +'\');'}
+		</div>"
+  end
   def show_admin_info_for(member, parent)
     return image_tag("admin/admin-div-element-sysadmin.png", 
                       :alt => "sysadmin", 
