a6e4761f5049fbca79470e570fe81895fcda053b
Francisco Tufro <contacto@franciscotufro.com.ar>
2009-08-24 17:09:08 +0000

    * Team View Refactoring.

--- test/unit/project_test.rb
+++ test/unit/project_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
+  fixtures :projects, :members, :teams
+  test 'find the team containing a member or return the first' do 
+    assert_equal teams(:widmore_team), projects(:find_the_island).team_including(members(:dfaraday))
+    assert_equal projects(:find_the_island).teams.first, projects(:find_the_island).team_including(members(:kausten))
+  end
 end
