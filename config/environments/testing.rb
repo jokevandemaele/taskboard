634306a2ad1b7b8eefb9cc26a03715407c8e1933
Francisco Tufro <contacto@franciscotufro.com.ar>
2009-09-18 13:29:07 +0000

    * Now testing environment is like production.

--- config/environments/testing.rb
+++ config/environments/testing.rb
@@ -1,28 +1,24 @@
 # Settings specified here will take precedence over those in config/environment.rb
 
-# The test environment is used exclusively to run your application's
-# test suite.  You never need to work with it otherwise.  Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs.  Don't rely on the data there!
+# The production environment is meant for finished, "live" apps.
+# Code is not reloaded between requests
 config.cache_classes = true
 
-# Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+# Enable threaded mode
+# config.threadsafe!
 
-# Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
-config.action_view.cache_template_loading            = true
+# Use a different logger for distributed setups
+# config.logger = SyslogLogger.new
 
-# Disable request forgery protection in test environment
-config.action_controller.allow_forgery_protection    = false
+# Full error reports are disabled and caching is turned on
+config.action_controller.consider_all_requests_local = false
+config.action_controller.perform_caching             = true
 
-# Tell Action Mailer not to deliver emails to the real world.
-# The :test delivery method accumulates sent emails in the
-# ActionMailer::Base.deliveries array.
-# config.action_mailer.delivery_method = :test
+# Use a different cache store in production
+# config.cache_store = :mem_cache_store
 
-# Use SQL instead of Active Record's schema dumper when creating the test database.
-# This is necessary if your schema can't be completely dumped by the schema dumper,
-# like if you have constraints or database-specific column types
-# config.active_record.schema_format = :sql
+# Enable serving of images, stylesheets, and javascripts from an asset server
+# config.action_controller.asset_host                  = "http://assets.example.com"
+
+# Disable delivery errors, bad email addresses will be ignored
+# config.action_mailer.raise_delivery_errors = false
