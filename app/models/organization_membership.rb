934478d0aff15f0aa840b2b043ef0ba1aacb23c0
marquete <marquete@gmail.com>
2009-09-22 05:38:48 +0000

    * First member in an organization administers it.

--- app/models/organization_membership.rb
+++ app/models/organization_membership.rb
@@ -2,11 +2,16 @@ class OrganizationMembership < ActiveRecord::Base
   belongs_to :organization
   belongs_to :member
 
+  before_create :make_first_member_admin
   after_create :add_member_to_default_team
 
   # IMPORTANT: This named scope references _ONLY_ the Organization Memberships, it doesn't takes in account if the user is a system admin.
   # If you want to get the organizations a user can administer use member.organizations_administered 
   named_scope :administered, :conditions => { :admin => true }
+
+  def make_first_member_admin
+    self.admin = true if organization.members.empty?
+  end
 
   def add_member_to_default_team
     organization.teams.first.members << member if organization.teams.size == 1
--- test/unit/organization_membership_test.rb
+++ test/unit/organization_membership_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class OrganizationMembershipTest < ActiveSupport::TestCase
+  test "first member added to an organization should be given admin status" do
+    others = Organization.create(:name => 'The Others')
+    one_john = Member.create(:name => 'John Locke', :username => 'jlocke', :password => 'secret', :email => 'jlocke@example.com')
+    another_john = Member.create(:name => 'John Locke', :username => 'johnl', :password => 'secret', :email => 'johnl@example.com')
+
+    OrganizationMembership.create(:member => one_john, :organization => others)
+    assert one_john.admins?(others)
+    OrganizationMembership.create(:member => another_john, :organization => others)
+    assert !another_john.admins?(others)
+  end
+
   test "adminitered namescope works as expected" do
     assert_equal 1, members(:cwidmore).organization_memberships.administered.size
     assert_equal 0, members(:dfaraday).organization_memberships.administered.size
