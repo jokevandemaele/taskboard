5a5934a330ba50dc3738b63e2599b47a52de4405
Francisco Tufro <contacto@franciscotufro.com.ar>
2009-09-24 22:50:10 +0000

    * Reduced taskboard and tasks.

--- app/helpers/taskboard_helper.rb
+++ app/helpers/taskboard_helper.rb
@@ -2,7 +2,7 @@ module TaskboardHelper
   def taskboard_border_style(color,position)
     text = ""
     position.each do |pos|
-      text << "border-#{pos}: 10px solid ##{color}; "
+      text << "border-#{pos}: 4px solid ##{color}; "
     end
     return text
   end
