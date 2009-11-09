6f3b04fff8d04a62f2318c5b6ad2359f37ad33d6
Francisco Tufro <contacto@franciscotufro.com.ar>
2009-11-09 21:33:48 +0000

    * Changed mail configuration.

--- config/environment.rb
+++ config/environment.rb
@@ -82,12 +82,16 @@ Rails::Initializer.run do |config|
   # ActionMailer
   #config.action_mailer.delivery_method = :sendmail
   config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #    :address => 'mail.agilar.org',
+  #    :port => 25,
+  #    :domain => 'agilar.org',
+  #    :authentication => :login,
+  #    :user_name => 'no-reply@agilar.org',
+  #    :password => 'agilarnrp.094'
+  # }
   config.action_mailer.smtp_settings = {
-     :address => 'mail.agilar.org',
+     :address => 'localhost',
      :port => 25,
-     :domain => 'agilar.org',
-     :authentication => :login,
-     :user_name => 'no-reply@agilar.org',
-     :password => 'agilarnrp.094'
   }
 end
