91d61792622f572ab74d049fa9f2ab93d16079b2
Francisco Tufró <nictuku@arandela.local>
2010-03-12 17:40:38 -0200

    Added blue-ridge to test javascript.

---
+++ public/javascripts/helpers/organizations.js
@@ -0,0 +1,17 @@
+Application.Helpers.Organizations = {
+  initialize: function(){
+    this._setupListeners();
+  },
+  
+  _setupListeners: function(){
+    // New Organization Listener
+    
+    // Edit Organization Listener
+    // Destroy Organization Listener
+    // New Project Listener
+  }
+  
+};
+
+// Provide a pretty shortcut
+Organizations = Application.Helpers.Organizations;
---
+++ public/javascripts/startup.js
@@ -0,0 +1,2 @@
+/* This JavaScript initializes the Application class, it should be called after loading all the helpers needed for the page */
+Application.initialize({});
