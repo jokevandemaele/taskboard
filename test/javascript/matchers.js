61abb90cfc81c488dbef9736f3dd0128d92cd638
Francisco Tufró <nictuku@arandela.local>
2010-03-14 13:03:04 -0200

    Removed matchers.js

--- test/javascript/matchers.js
+++
@@ -1,8 +0,0 @@
-Screw.Matchers["be_shown"] = {
-  match: function(actual) {
-    return actual.visible == true;
-  },
-  failure_message: function(expected, actual, not) {
-    return 'expected ' + $.print(actual) + 'to be shown';
-  }
-}
