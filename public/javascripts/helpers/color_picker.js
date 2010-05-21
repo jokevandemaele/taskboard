ab7b0302d9806e5c27637ef63e0e8c89feb22ab6
marquete <marquete@ngr.(none)>
2010-05-21 02:39:20 -0300

    Did some further refactoring on the color picker and applied it to teams.

--- public/javascripts/helpers/color_picker.js
+++ public/javascripts/helpers/color_picker.js
@@ -1,16 +1,42 @@
 Application.Helpers.ColorPicker = {
   initialize: function() {
+    var colors = ['00ff00', '32cd32', '228b57', '3cb371', '00fa9a', '66cdaa',
+                  '9acd32', 'ffd700', 'ffff00', 'eee8aa', 'f0e68c', 'bdb76b',
+                  'ff69b4', 'ff1493', 'ff7f50', 'ff6347', 'dc143c', 'ff0000',
+                  'd8bfd8', 'da70d6', 'ff00ff', 'ba55d3', '9966cc', '800080',
+                  'b0c4de', 'add8e6', '00bfff', '1e90ff', '0000ff', '4682b4',
+                  '7b68ee', '6a5acd', '483d8b', 'c0c0c0', 'a9a9a9', '708090'];
+
     this.colorPicker = new Element('div').writeAttribute(
       {'id': 'color-picker'}
     ).setStyle(
       {position: 'absolute', top: 0, left: 0, display: 'none'}
+    ).update(
+      new Element('table').setStyle(
+        {'border-spacing': '4px', 'background': 'white'}
+      )
     );
+
+    for(var i = 0; i < 6; i++) {
+      var tr = new Element('tr');
+      for(var j = 0; j < 6; j++) {
+        var td = new Element('td').writeAttribute(
+          {'class': 'color-picker-cell'}
+        ).setStyle(
+          {'width': '24px', 'height': '24px', 'background': '#' + colors[i * 6 + j]}
+        );
+        tr.appendChild(td);
+      }
+      this.colorPicker.down('table').appendChild(tr);
+    }
   },
 
-  toggle: function() {
-    this.colorPicker.setStyle({
-      display: ((this.colorPicker.getStyle('display') == 'none') ? '' : 'none')
-    });
+  _show: function() {
+    this.colorPicker.setStyle({display: ''});
+  },
+
+  _hide: function() {
+    this.colorPicker.setStyle({display: 'none'});
   },
 
   _dec2hex: function(dec) {
@@ -26,15 +52,23 @@ Application.Helpers.ColorPicker = {
   attach: function(parent, top, left, target) {
     var _this = this;
 
-    this.colorPicker.setStyle({'top': top + 'px', 'left': left + 'px'});
-    this.colorPicker.innerHTML = this.generateColorTable(target);
+    this.colorPicker.setStyle({'top': top, 'left': left});
     $(parent).appendChild(this.colorPicker);
-    $(target).observe('click', function(event) {
-      _this.toggle();
+
+    $(document.body).observe('click', function(event) {
+      var eventTarget = event.element();
+      if(eventTarget.match('#' + target))
+        _this._show();
+      else {
+        if(eventTarget.match('.color-picker-cell'))
+          _this._setColor(eventTarget, $(target));
+        else
+          _this._hide();
+      }
     });
   },
 
-  setColor: function(htmlObject, targetObject) {
+  _setColor: function(htmlObject, targetObject) {
     var color = htmlObject.style.backgroundColor;
     if(color.charAt(0) == '#')
       color = color.substr(1);
@@ -42,29 +76,6 @@ Application.Helpers.ColorPicker = {
       color = this._rgb2hex(color);
     $(targetObject).style.backgroundColor = $(targetObject).style.color = '#' + color;
     $(targetObject).value = color;
-  },
-
-  generateColorTable: function(targetObject) {
-    var content = '<table style="border-spacing: 4px; background: white;">';
-    var color;
-    var i, j, k, colors_index = 0;
-    var colors = ['00ff00', '32cd32', '228b57', '3cb371', '00fa9a', '66cdaa',
-                  '9acd32', 'ffd700', 'ffff00', 'eee8aa', 'f0e68c', 'bdb76b',
-                  'ff69b4', 'ff1493', 'ff7f50', 'ff6347', 'dc143c', 'ff0000',
-                  'd8bfd8', 'da70d6', 'ff00ff', 'ba55d3', '9966cc', '800080',
-                  'b0c4de', 'add8e6', '00bfff', '1e90ff', '0000ff', '4682b4',
-                  '7b68ee', '6a5acd', '483d8b', 'c0c0c0', 'a9a9a9', '708090'];
-
-    colors_index = 0;
-    for(i = 0; i < 6; i++) {
-      content += '<tr>';
-      for(j = 0; j < 6; j++) {
-            content += '<td onclick="ColorPicker.setColor(this, \'' + targetObject + '\');" style="background: #' + colors[colors_index++] + '; width: 24px; height: 24px;">&nbsp;</td>';
-      }
-      content += '</tr>';
-    }
-    content += '</table>';
-    return content;
   }
 };
 
