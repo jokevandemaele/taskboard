84fc8594beeacf0f30f92fced4ee0e896e581386
Francisco Tufró <nictuku@arandela.local>
2010-06-11 17:15:36 -0300

    Migrated to 2.3.8

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -68,7 +68,7 @@ Rails::Initializer.run do |config|
   # Make sure the secret is at least 30 characters and all random, 
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => '_taskboard_session',
+    :key => '_taskboard_session',
     :secret      => '3cd83128b90a6dc2cbaf748b67403769f34ff9beba9e4f46d044699bbc8c59a576a311b79318eb763a44dbc68e1490502c227a2fcf9aab037404f00a23704c55'
   }
 
