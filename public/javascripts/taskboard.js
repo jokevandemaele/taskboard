9be1a0e1c505b6a01d0f154f9f6018e493fe842d
Francisco Tufró <nictuku@arandela.local>
2010-07-05 13:33:22 -0300

    Enhanced nametags. Now we don't use images for them.

--- public/javascripts/taskboard.js
+++ public/javascripts/taskboard.js
@@ -234,11 +234,10 @@ var DDM = YAHOO.util.DragDropMgr;
            Dom.removeClass(dragEl, dragEl.className);
            Dom.addClass(dragEl, 'nametag')
            Dom.setStyle(dragEl, "border", "none"); 
-           Dom.setStyle(dragEl, "width", "60px");
+           Dom.setStyle(dragEl, "min-width", "58px");
            Dom.setStyle(dragEl, "height", "12px");
            Dom.setStyle(dragEl, "background-color", clickEl.getStyle('background-color'));
            Dom.setStyle(dragEl, "background-image", clickEl.getStyle('background-image'));
-           
          }
        },
 
